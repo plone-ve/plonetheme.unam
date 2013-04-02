4cb5344c99633ebae0b0bd7521f49d4c8298938e
Gildardo Bautista <gil.cano@gmail.com>
2013-04-02 12:30:02 -0600

    Revert "Institution logo redirect to main page"
    This reverts commit ca21a85c3f3be65a35d775895780978eb186fdd5.

--- src/plonetheme/unam/testing.py
+++ src/plonetheme/unam/testing.py
@@ -11,8 +11,7 @@ class Theming(PloneSandboxLayer):
     def setUpZope(self, app, configurationContext):
         #load ZCML
         import plonetheme.unam
-        xmlconfig.file(
-            'configure.zcml', plonetheme.unam, context=configurationContext)
+        xmlconfig.file('configure.zcml', plonetheme.unam, context=configurationContext)
 
     def setUpPloneSite(self, portal):
         pass
